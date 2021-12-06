82b7d74a1494e8ba77c7ed71b9f94a50b3741e69
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2021-12-06 19:38:13 +0700

    Fragment

---
+++ DialogFragment/app/src/main/java/com/android45/dialogfragment/HomeFragment.java
@@ -0,0 +1,4 @@
+package com.android45.dialogfragment;
+
+public class HomeFragment {
+}
--- DialogFragment/app/src/main/java/com/android45/dialogfragment/MainActivity.java
+++ DialogFragment/app/src/main/java/com/android45/dialogfragment/MainActivity.java
@@ -1,14 +1,30 @@
 package com.android45.dialogfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.android45.dialogfragment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        activityMainBinding.tvName.setText("Truong Trung Kien");
+        activityMainBinding.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), AddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
